1045e6717b54da29f06f8c046743d828c12b293a
unknown <oscar.charo09@gmail.com>
2019-03-25 23:08:17 -0500

    more stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,122 @@
+$(document).ready(funtion()) {
+    const timer =30;
+    const currentQuestion=0
+    const choices = [];
+    const userGuess=0;
+    const correctAnswer=0;
+}
+const questions =[
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+
+    {
+        question: "?",
+        answers: {
+            
+            a:"a",
+            b:"b",
+            c:"c",
+            d:"d",
+        },
+    },
+];
+  
+$("#start").on("click",function(){
+    Game.reset();
+});
